e9844a629af010fe58b22c7abc934440318287f7
Rankov Julia <nikypod@gmail.com>
2025-08-11 10:53:07 +0300

    logger added

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,18 @@ import express, {Request, Response, NextFunction} from 'express';
 import {PORT} from "./config/libConfig.ts";
 import {libRouter} from "./routes/libRouter.ts";
 import {errorHandler} from "./errorHandler/errorHandler.ts";
+import morgan from "morgan";
+import * as fs from "node:fs";
 
 export const launchServer = () => {
     const app = express();
     app.listen(PORT, () => console.log(`Server runs at http://localhost:${PORT}`));
+    const logStream = fs.createWriteStream("access.log", {flags: "a"});
 
     //MIDDLEWARE
     app.use(express.json()); // parse stream into json
+    app.use(morgan("dev")); // token: "dev", "short"
+    app.use(morgan("combined", {stream: logStream}));
 
     //ROUTER
     app.use('/api', libRouter);
